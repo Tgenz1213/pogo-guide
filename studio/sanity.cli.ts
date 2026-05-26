3884ded4319b43176fbf72c44349436d6923de05
Timothy Genz <34835862+Tgenz1213@users.noreply.github.com>
2026-05-26 08:21:20 -0500

    chore: Add appId for deployments

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -3,7 +3,7 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: '84tfhiiz',
-    dataset: 'production'
+    dataset: 'production',
   },
   deployment: {
     /**
@@ -11,5 +11,6 @@ export default defineCliConfig({
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
      */
     autoUpdates: true,
-  }
+    appId: 'pay2m1ro3a2hg9q5t6a1khop',
+  },
 })
